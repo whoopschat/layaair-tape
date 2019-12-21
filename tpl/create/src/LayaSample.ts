fa527231d0d10658c19e12dcfe142eab3a42fcc8
liangmayong <liangmayong@qutoutiao.net>
2019-12-21 16:45:00 +0800

    完善模版

--- tpl/create/src/LayaSample.ts
+++ tpl/create/src/LayaSample.ts
@@ -1,15 +1,17 @@
 // 游戏入口
 class GameMain {
     constructor() {
+        // 初始化场景
         Tape.init(750, 1334);
-        Tape.bg.setBgColor('#3399ff');
+        // 设置纯色背景
+        // Tape.bg.setBgColor('#3399ff');
+        // 设置贴纸背景
+        Tape.bg.setBgTexture('res/unpack/bg.png');
+        // 开始加载游戏
         Tape.start({
             mainPage: LoadingActivity,
             commonRes: [
-                { url: 'res/atlas/comp.atlas', type: Laya.Loader.ATLAS },
-                { url: 'res/1.jpg', type: Laya.Loader.IMAGE },
-                { url: 'res/2.jpg', type: Laya.Loader.IMAGE },
-                { url: 'res/3.jpg', type: Laya.Loader.IMAGE },
+                { url: 'res/atlas/comp.atlas', type: Laya.Loader.ATLAS }
             ]
         });
     }
